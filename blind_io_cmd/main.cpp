8d579932d0c24435d652beb346669218eab8631b
Nathan Baggs <nathan.baggs.contact@gmail.com>
2024-04-27 19:01:19 +0100

    Initial commit

---
+++ blind_io_cmd/main.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+//         Distributed under the Boost Software License, Version 1.0.         //
+//            (See accompanying file LICENSE or copy at                       //
+//                 https://www.boost.org/LICENSE_1_0.txt)                     //
+////////////////////////////////////////////////////////////////////////////////
+
+#include <print>
+
+#include "test.h"
+
+int main()
+{
+    std::println("{}", bio::message());
+
+    return 0;
+}
