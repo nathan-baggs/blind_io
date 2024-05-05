542191136102e58d328a1146dc70d8e06b074bcc
Nathan Baggs <nathan.baggs.contact@gmail.com>
2024-05-05 10:02:12 +0100

    Integrate changes into cmd project

--- blind_io_cmd/main.cpp
+++ blind_io_cmd/main.cpp
@@ -15,6 +15,10 @@ int main()
     for (const auto &proc : bio::find_process("firefox.exe"))
     {
         std::println("{} -> {}", proc.name(), proc.pid());
+        for (const auto &region : proc.memory_regions())
+        {
+            std::println("0x{:x} 0x{:x} {}", region.address(), region.size(), region.protection());
+        }
     }
 
     return 0;
