bea52c6f39d815366537bc5152049807cc6b056f
Nathan Baggs <nathan.baggs.contact@gmail.com>
2024-05-16 20:23:43 +0100

    Add find and replace utility function

--- blind_io_lib/include/blind_io/process_utils.h
+++ blind_io_lib/include/blind_io/process_utils.h
@@ -6,11 +6,22 @@
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <optional>
+#include <ranges>
+#include <span>
 #include <string_view>
+#include <type_traits>
 #include <vector>
 
 #include "process.h"
+
+// concept to constrain a type to a character type
+template <class T>
+concept Character =
+    std::same_as<T, char> || std::same_as<T, signed char> || std::same_as<T, unsigned char> ||
+    std::same_as<T, wchar_t> || std::same_as<T, char8_t> || std::same_as<T, char16_t> || std::same_as<T, char32_t>;
 
 namespace bio
 {
@@ -30,5 +41,62 @@ std::vector<std::uint32_t> get_pids();
  *   Collection of processes, empty if none were found (or could be opened).
  */
 std::vector<Process> find_process(std::string_view name);
+
+/**
+ * Replace a given memory pattern with another.
+ *
+ * @param process
+ *   The process to replace memory in.
+ *
+ * @param region
+ *   The region to replace memory in (must be read writable),
+ *
+ * @param find
+ *   The byte pattern to find.
+ *
+ * @param replace
+ *   The byte pattern to replace "find" with (must be the same length as "find").
+ *
+ * @param num_occurrences
+ *   If supplied the maximum number of times to replace "find" (all if not supplied).
+ */
+void replace_memory(
+    const Process &process,
+    const MemoryRegion &region,
+    std::span<const std::uint8_t> find,
+    std::span<const std::uint8_t> replace,
+    std::optional<std::size_t> num_occurrences = std::nullopt);
+
+// specialisations of replace_memory for various other types
+
+template <class R>
+void replace_memory(
+    const Process &process,
+    const MemoryRegion &region,
+    R &&find,
+    R &&replace,
+    std::optional<std::size_t> num_occurrences = std::nullopt) requires std::ranges::contiguous_range<R>
+{
+    const auto find_bytes = reinterpret_cast<const std::uint8_t *>(std::ranges::data(find));
+    const auto replace_bytes = reinterpret_cast<const std::uint8_t *>(std::ranges::data(replace));
+
+    replace_memory(
+        process,
+        region,
+        {find_bytes, std::ranges::size(find) * sizeof(std::ranges::range_value_t<R>)},
+        {replace_bytes, std::ranges::size(replace) * sizeof(std::ranges::range_value_t<R>)},
+        num_occurrences);
+}
+
+template <Character T, std::size_t N>
+void replace_memory(
+    const Process &process,
+    const MemoryRegion &region,
+    const T (&find)[N],
+    const T (&replace)[N],
+    std::optional<std::size_t> num_occurrences = std::nullopt)
+{
+    replace_memory(process, region, std::span{find, N - 1}, std::span{replace, N - 1}, num_occurrences);
+}
 
 }
--- blind_io_lib/process_utils.cpp
+++ blind_io_lib/process_utils.cpp
@@ -6,11 +6,19 @@
 
 #include "process_utils.h"
 
+#include <cassert>
+#include <cstddef>
 #include <cstdint>
+#include <limits>
+#include <optional>
 #include <ranges>
+#include <span>
 #include <stdexcept>
+#include <string_view>
 #include <vector>
 
+
+#define NOMINMAX
 #include <Windows.h>
 
 #include <Psapi.h>
@@ -66,6 +74,41 @@ std::vector<Process> find_process(std::string_view name)
     }
 
     return procs;
+}
+
+void replace_memory(
+    const Process &process,
+    const MemoryRegion &region,
+    std::span<const std::uint8_t> find,
+    std::span<const std::uint8_t> replace,
+    std::optional<std::size_t> num_occurrences)
+{
+    assert(find.size() == replace.size());
+    assert(region.test_protection(MemoryRegionProtection::READ | MemoryRegionProtection::WRITE));
+
+    auto mem = process.read(region);
+    auto mem_span = std::span(mem);
+
+    auto remaining_ocurrences = num_occurrences.value_or(std::numeric_limits<std::size_t>::max());
+
+    while (!mem_span.empty() && (remaining_ocurrences > 0))
+    {
+        if (const auto found = std::ranges::search(mem_span, find); !found.empty())
+        {
+            const auto begin = std::ranges::distance(std::cbegin(mem_span), std::cbegin(found));
+
+            std::memcpy(mem_span.data() + begin, replace.data(), replace.size());
+
+            process.write(region, mem);
+
+            mem_span = mem_span.subspan(begin);
+            --remaining_ocurrences;
+        }
+        else
+        {
+            break;
+        }
+    }
 }
 
 }
