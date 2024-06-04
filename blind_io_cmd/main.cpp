fd785a604682a57e78c0641dfc964e656d48eaa7
iris dev <iris-engine-dev@gmail.com>
2024-06-04 18:50:43 +0100

    Integrate changes into app

--- blind_io_cmd/main.cpp
+++ blind_io_cmd/main.cpp
@@ -4,12 +4,16 @@
 //                 https://www.boost.org/LICENSE_1_0.txt)                     //
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <cassert>
+#include <chrono>
 #include <print>
 #include <ranges>
 #include <span>
 #include <stdexcept>
 #include <string_view>
+#include <thread>
 
+#include "debugger.h"
 #include "process.h"
 #include "process_utils.h"
 
@@ -17,17 +21,37 @@ using namespace std::literals;
 
 int main()
 {
-    for (const auto &proc : bio::find_process("Notepad.exe"))
+    auto procs = bio::find_process("vim");
+    assert(procs.size() == 1);
+
+    auto &proc = procs.front();
+
+    auto tid = proc.pid();
+
+    std::println("{} -> {}", proc.name(), proc.pid());
+
+    for (const auto &thread : proc.threads())
     {
-        std::println("{} -> {}", proc.name(), proc.pid());
-        for (const auto &region : proc.memory_regions())
-        {
-            if (region.test_protection(bio::MemoryRegionProtection::READ | bio::MemoryRegionProtection::WRITE))
-            {
-                bio::replace_memory(proc, region, L"vorpal", L"VORPAL", 1);
-            }
-        }
+        std::println("tid: {}", thread.tid());
     }
+
+    bio::Debugger dbg{std::move(proc)};
+
+    [[maybe_unused]] const auto regs = dbg.registers(tid);
+
+    std::this_thread::sleep_for(10s);
+
+    // for (const auto &proc : bio::find_process("vim"))
+    //{
+    //     std::println("{} -> {}", proc.name(), proc.pid());
+    //     for (const auto &region : proc.memory_regions())
+    //     {
+    //         if (region.test_protection(bio::MemoryRegionProtection::READ | bio::MemoryRegionProtection::WRITE))
+    //         {
+    //             bio::replace_memory(proc, region, "vorpal", "VORPAL");
+    //         }
+    //     }
+    // }
 
     return 0;
 }
